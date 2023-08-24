7dceaed078bc98796b8c2baa0cd282347ecacc5d
Nathan Graule <solarliner@gmail.com>
2023-08-24 22:45:52 +0000

    "wip: switch nixos-wsl to flakes"

--- system/wsl/configuration.nix
+++ system/wsl/configuration.nix
@@ -1,12 +1,8 @@
 { lib, pkgs, config, modulesPath, ... }:
 with lib;
-let
-  nixos-wsl = import ./nixos-wsl;
-in
 {
   imports = [
     "${modulesPath}/profiles/minimal.nix"
-    nixos-wsl.nixosModules.wsl
   ];
 
   wsl = {
--- system/wsl/flake.nix
+++ system/wsl/flake.nix
@@ -1,13 +1,17 @@
 {
   description = "NixOS configuration for WSL";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixos-wsl.url = github:nix-community/nixos-wsl/22.05-5c211b47;
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nixos-wsl }: {
     nixosConfigurations = {
       wsl-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        modules = [ ./configuration.nix ];
+        modules = [
+          nixos-wsl.nixosModules.default
+          ./configuration.nix
+        ];
       };
     };
   };
