1f09e45d255fff444b0a484854388a1037afd439
Nathan Graule <solarliner@gmail.com>
2024-02-26 23:02:13 +0100

    feat(nixos): add auto upgrade

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{ inputs, outputs, lib, config, pkgs, ... }: {
   imports = [
     ./users
     ./boot.nix
@@ -18,7 +11,7 @@
   ];
   nixpkgs = {
     # You can add overlays here
-    overlays = [];
+    overlays = [ ];
     # Configure your nixpkgs instance
     config = {
       # Disable if you don't want unfree packages
@@ -28,23 +21,20 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry =
-    (lib.mapAttrs (_: flake: {inherit flake;}))
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; }))
     ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs' (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
+  nix.nixPath = [ "/etc/nix/path" ];
+  environment.etc = lib.mapAttrs' (name: value: {
+    name = "nix/path/${name}";
+    value.source = value.flake;
+  }) config.nix.registry;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [wget curl neovim];
+  environment.systemPackages = with pkgs; [ wget curl neovim ];
 
   nix.settings = {
     # Enable flakes and new 'nix' command
@@ -56,6 +46,21 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
+  };
+
+  # https://nixos.wiki/wiki/Automatic_system_upgrades
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "--update-input"
+      "nixpkgs-unstable"
+      "-L" # print build logs
+    ];
+    dates = "18:00";
+    randomizedDelaySec = "45min";
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
