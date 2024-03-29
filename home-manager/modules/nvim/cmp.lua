fc34896b20980b30282de13e1dc9762013e2f2fe
Nathan Graule <nathan.graule@arturia.com>
2024-03-29 19:42:40 +0100

    system= 7 2024 03 25  | home-manager=2024-03-29 19:41 : id 23

--- home-manager/modules/nvim/cmp.lua
+++ home-manager/modules/nvim/cmp.lua
@@ -113,5 +113,5 @@ require "nvim-autopairs".setup {}
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
 cmp.event:on(
     'confirm_done',
-    cmp_autopairs.on_conrifm_done()
+    cmp_autopairs.on_confirm_done()
 )
