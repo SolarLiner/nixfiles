db875295fe3147b61b574646eb13065514c0a946
Nathan Graule <nathan.graule@arturia.com>
2024-03-29 19:54:56 +0100

    fix(neovim): nvim-tree accepts commands again

--- home-manager/modules/nvim/nvim-tree.lua
+++ home-manager/modules/nvim/nvim-tree.lua
@@ -23,14 +23,6 @@ require 'nvim-tree'.setup {
             "Makefile",
         },
     },
-    on_attach = function(bufnr)
-        local function opts(desc)
-            return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-        local ok, api = pcall(require, "nvim-tree.api")
-        assert(ok, "api module is not found")
-        vim.keymap.set("n", "<CR>", api.node.open.tab_drop, opts("Tab drop"))
-    end
 }
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
