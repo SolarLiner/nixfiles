f44b37b5c14f617ca9b9516d4ecc6dd3acc1d8e4
Nathan Graule <nathan.graule@arturia.com>
2024-04-15 23:06:45 +0200

    system= 10 2024 04 15  | home-manager=2024-04-15 23:05 : id 47

--- home-manager/modules/nvim/plugins/cmp.lua
+++ home-manager/modules/nvim/plugins/cmp.lua
@@ -2,6 +2,7 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
+      "windwp/nvim-autopairs",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
--- home-manager/modules/nvim/plugins/init.lua
+++ home-manager/modules/nvim/plugins/init.lua
@@ -2,6 +2,13 @@ return {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   "pocco81/auto-save.nvim",
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
