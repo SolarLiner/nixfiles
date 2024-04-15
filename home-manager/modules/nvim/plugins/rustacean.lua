35498112f69f23da8dbe29f674f1a7ed533cac07
Nathan Graule <nathan.graule@arturia.com>
2024-04-15 15:09:07 +0200

    system= 10 2024 04 15  | home-manager=2024-04-15 15:09 : id 43

---
+++ home-manager/modules/nvim/plugins/rustacean.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4", -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<leader>rc", function()
+              vim.cmd.RustLsp("openCargo")
+            end, {
+              desc = "[R]ust open [C]argo.toml",
+            })
+            vim.keymap.set("n", "<leader>rr", function()
+              vim.cmd.RustLsp("run")
+            end, {
+              desc = "[R]ust [R]un target under cursor",
+            })
+            vim.keymap.set("n", "<leader>rt", function()
+              vim.cmd.RustLsp("testables")
+            end, {
+              desc = "[R]ust [T]est",
+            })
+          end,
+        },
+      }
+    end,
+  },
+}
