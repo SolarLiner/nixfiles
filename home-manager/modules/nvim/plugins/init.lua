00cc9a20e89d6713a7fa645d1cf2cd7761dff4c2
Nathan Graule <nathan.graule@arturia.com>
2024-04-16 08:49:04 +0200

    system= 10 2024 04 15  | home-manager=2024-04-16 08:48 : id 48

--- home-manager/modules/nvim/plugins/init.lua
+++ home-manager/modules/nvim/plugins/init.lua
@@ -71,6 +71,26 @@ return {
     end,
   },
 
+  {
+    "klen/nvim-config-local",
+    event = "VimEnter",
+    config = function()
+      require("config-local").setup({
+        -- Default options (optional)
+
+        -- Config file patterns to load (lua supported)
+        config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+
+        -- Where the plugin keeps files data
+        hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+        autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+        commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+        silent = false, -- Disable plugin messages (Config loaded/ignored)
+        lookup_parents = false, -- Lookup config files in parent directories
+      })
+    end,
+  },
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
