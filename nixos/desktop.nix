0bea7e56a9e5b8a8611805a0cc987ce00dfccf2c
Nathan Graule <solarliner@gmail.com>
2024-05-12 11:23:31 +0200

    system=96 current 2024 05 12  | home-manager=2024-05-10 01:42 : id 57

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -15,7 +15,7 @@
       displayManager = {
         sddm.enable = true;
         sddm.wayland.enable = true;
-        defaultSession = "plasmawayland";
+        defaultSession = "plasma";
       };
       desktopManager.plasma5.enable = true;
     };
