595cce3578007031e1ea8fcb0aae14f4ad96d0ae
Nathan Graule <nathan.graule@arturia.com>
2024-05-14 10:18:20 +0200

    chore: formatting

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,7 +11,11 @@
     };
 in {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = _final: prev: import ../pkgs { inherit inputs; pkgs = prev; };
+  additions = _final: prev:
+    import ../pkgs {
+      inherit inputs;
+      pkgs = prev;
+    };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
