200ee500847d38fac13f2e1018f6f8b450d8a539
Nathan Graule <nathan.graule@arturia.com>
2024-05-14 10:00:34 +0200

    chore: shell script lint fix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,9 +7,9 @@ with pkgs; {
     runtimeInputs = [git gnumake];
     text = ''
       NIXFILES_DIR=$HOME/.config/nixfiles
-      mkdir -p $NIXFILES_DIR
-      git clone https://github.com/solarliner/nixfiles.git $NIXFILES_DIR
-      make -C $NIXFILES_DIR home
+      mkdir -p "$NIXFILES_DIR"
+      git clone https://github.com/solarliner/nixfiles.git "$NIXFILES_DIR"
+      make -C "$NIXFILES_DIR" home
     '';
   };
   clangd = callPackage ./clangd.nix {};
