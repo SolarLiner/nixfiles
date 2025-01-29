4a4215189c1e4eb515e928e45aecabfa64270fc1
Nathan Graule <nathan.graule@arturia.com>
2025-01-29 14:26:03 +0100

    feat(shell): update starship config in nushell

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -56,15 +56,10 @@ in {
   };
   programs.nushell = {
     enable = true;
-    envFile = {
-      text = ''
-        mkdir ~/.cache/starship
-        starship init nu | save ~/.cache/starship/init.nu
-      '';
-    };
     configFile = {
-      text = ''
-        source ~/.cache/starship/init.nu
+      text = lib.mkIf config.programs.starship.enable ''
+        mkdir ($nu.data-dir | path join "vendor/autoload")
+        starship init nu | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")
       '';
     };
   };
