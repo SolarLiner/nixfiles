d3c2e03b98532be187dcc7188278f0f2f4ab96ee
Nathan Graule <nathan.graule@arturia.com>
2025-04-03 09:43:38 +0200

    system= 78 2025 03 31  | home-manager=2025-04-03 09:43 : id 164

--- home-manager/modules/topgrade.nix
+++ home-manager/modules/topgrade.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       misc.assume_yes = true;
-      misc.disable = ["brew_cask" "brew_formula" "flatpak" "home_manager" "node" "gem" "poetry"];
+      misc.disable = ["brew_cask" "brew_formula" "flatpak" "home_manager" "node" "gem" "poetry" "mas"] ++ lib.optionals pkgs.stdenv.isDarwin ["system"];
       commands = {
         "home-manager" = let
           extraTargets = lib.optionals pkgs.stdenv.isDarwin ["system"];
