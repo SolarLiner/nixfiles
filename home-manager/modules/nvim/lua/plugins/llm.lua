e0b94341d0d250d6269269ff3c7ca67fe4809019
Nathan Graule <solarliner@gmail.com>
2025-04-15 12:52:28 +0200

    system= 13 2025 04 14  | home-manager=2025-04-15 12:52 : id 18

--- home-manager/modules/nvim/lua/plugins/llm.lua
+++ home-manager/modules/nvim/lua/plugins/llm.lua
@@ -10,14 +10,15 @@ return {
     opts = {
       -- add any opts here
       -- for example
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      provider = "mistral",
+      vendors = {
+        mistral = {
+          __inherited_from = "openai",
+          api_key_name = "MISTRAL_API_KEY",
+          endpoint = "https://api.mistral.ai/v1/",
+          model = "mistral-large-latest",
+          max_tokens = 4096, -- to avoid using max_completion_tokens
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
