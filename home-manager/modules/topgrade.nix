cbe681476362ce09557924af1c432100789fe566
Nathan Graule <nathan.graule@arturia.com>
2025-04-22 11:11:31 +0200

    system= 88 2025 04 22  | home-manager=2025-04-22 09:33 : id 171

--- home-manager/modules/topgrade.nix
+++ home-manager/modules/topgrade.nix
@@ -4,12 +4,6 @@
     settings = {
       misc.assume_yes = true;
       misc.disable = ["brew_cask" "brew_formula" "flatpak" "home_manager" "node" "gem" "poetry" "mas"] ++ lib.optionals pkgs.stdenv.isDarwin ["system"];
-      commands = {
-        "home-manager" = let
-          extraTargets = lib.optionals pkgs.stdenv.isDarwin ["system"];
-          targets = ["update" "home"] ++ extraTargets;
-        in lib.strings.concatStringsSep " " (["make -C ~/.config/nixfiles/"] ++ targets);
-      };
     };
   };
 }
