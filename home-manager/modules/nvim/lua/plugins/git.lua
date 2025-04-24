5778d0bd92f612c58c3011c61b17cc9221de3563
Nathan Graule <nathan.graule@arturia.com>
2025-04-24 15:58:55 +0200

    system= 88 2025 04 22  | home-manager=2025-04-24 15:58 : id 180

--- home-manager/modules/nvim/lua/plugins/git.lua
+++ home-manager/modules/nvim/lua/plugins/git.lua
@@ -1,6 +1,7 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     "lewis6991/gitsigns.nvim",
+    lazy = false,
     opts = {
       current_line_blame = true,
     },
