a97c73e3de22cd289762cca5fcbd6856368c49be
Nathan Graule <solarliner@gmail.com>
2025-04-24 19:42:47 +0200

    system= 20 2025 04 24  | home-manager=2025-04-24 19:41 : id 29

---
+++ home-manager/modules/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    lazy = true,
+    ft = { "markdown" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icon" }
+  },
+}
