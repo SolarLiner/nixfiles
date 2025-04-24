e994967c269d3fa473f04de2a73af8a16049ccb4
Nathan Graule <solarliner@gmail.com>
2025-04-24 20:05:32 +0200

    system= 20 2025 04 24  | home-manager=2025-04-24 20:05 : id 31

--- home-manager/modules/nvim/lua/plugins/oil.lua
+++ home-manager/modules/nvim/lua/plugins/oil.lua
@@ -1,7 +1,23 @@
 return {
   {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        -- "permissions",
+        "size",
+        "mtime",
+      },
+      watch_for_changes = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = "fast",
+        is_hidden_file = function ()
+          return false
+        end
+      },
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     cmd = "Oil",
