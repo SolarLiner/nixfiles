1bb70ec941083e9dd06f448fbf33910daa278ad4
Nathan Graule <solarliner@gmail.com>
2025-05-26 14:39:50 +0200

    system= | home-manager=2025-05-26 14:39 : id 44

--- home-manager/modules/plasma.nix
+++ home-manager/modules/plasma.nix
@@ -1,4 +1,10 @@
 {pkgs, ...}: {
   imports = [./plasma.generated.nix];
-  home.packages = with pkgs; [iosevka nerd-fonts inter];
+  home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
+    nerd-fonts.iosevka-term-slab
+    inter
+  ];
 }
