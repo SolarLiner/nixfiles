6728ed8cf4132b10970a04095db743f775770821
Nathan Graule <nathan.graule@arturia.com>
2025-07-07 11:40:37 +0200

    feat(intellij): which-key and switch plugin configuration

--- home-manager/modules/intellij.nix
+++ home-manager/modules/intellij.nix
@@ -1,37 +1,41 @@
 {
-  config,
-  lib,
   pkgs,
-  isWSL ? false,
   ...
 }: let
-  inherit (lib) mkIf;
   inherit (pkgs) stdenv;
+  toolboxPath =
+    if stdenv.isDarwin
+    then "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
+    else "$HOME/.local/share/JetBrains/Toolbox/scripts";
 in {
   home.file.".ideavimrc" = {
     text = ''
       set scrolloff=5
       set clipboard=unnamedplus
       set clipboard+=ideaput
+
       set command
       set commentary
       set easymotion
       set highlightedyank
       set incsearch
       set surround
+
+      set switch
+      nnoremap <leader>s :Switch<CR>
+      nnoremap <leader>S :SwitchReverse<CR>
+
+      set which-key
+      set notimeout
+      set timeoutlen=5000
     '';
   };
-  programs.zsh.initContent = let
-    toolboxPath =
-      if stdenv.isDarwin
-      then "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-      else "$HOME/.local/share/JetBrains/Toolbox/scripts";
-  in ''
+  programs.zsh.initContent = ''
     export PATH="${toolboxPath}:$PATH"
   '';
   home.sessionVariables = {
     _JAVA_AWT_WM_NONREPARENTING = "1";
-    JETBRAINS_TOOLBOX_HOME = "$HOME/.local/share/JetBrains/Toolbox/";
+    JETBRAINS_TOOLBOX_HOME = toolboxPath;
   };
-  home.sessionPath = ["$JETBRAINS_TOOLBOX_HOME/scripts"];
+  home.sessionPath = ["${toolboxPath}/scripts"];
 }
