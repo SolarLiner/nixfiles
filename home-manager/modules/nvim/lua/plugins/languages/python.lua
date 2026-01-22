7646bab5a376585ad0ec6d126a8c57b2425362f2
Nathan Graule <solarliner@gmail.com>
2026-01-22 23:25:33 +0100

    fix(nvim): remove errors when opening python files

--- home-manager/modules/nvim/lua/plugins/languages/python.lua
+++ home-manager/modules/nvim/lua/plugins/languages/python.lua
@@ -10,20 +10,6 @@ return {
     end,
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = { "python" },
-    opts = function()
-      local null_ls = require("null-ls")
-      return {
-        sources = {
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.ruff,
-          null_ls.builtins.formatting.mypy,
-        },
-      }
-    end,
-  },
-  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
