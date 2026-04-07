f4980e72101b69dfdb33edbb9fe50f6bef0b8a66
Nathan Graule <solarliner@gmail.com>
2026-04-07 13:11:01 +0200

    feat(neovim): whichpy

--- home-manager/modules/nixvim/languages/python.nix
+++ home-manager/modules/nixvim/languages/python.nix
@@ -3,4 +3,6 @@
   plugins.lsp.servers.basedpyright.enable = true;
   plugins.neotest.adapters.python.enable = true;
   plugins.dap-python.enable = true;
+  plugins.whichpy.enable = true;
+  plugins.whichpy.neotestIntegration = true;
 }
