265f3c31e33010e4726975b5391647a37a004368
Nathan Graule <nathan.graule@arturia.com>
2026-04-09 08:34:50 +0200

    feat(neovim): highlighted yank

--- home-manager/modules/nixvim/default.nix
+++ home-manager/modules/nixvim/default.nix
@@ -96,4 +96,16 @@
     updatetime = 250;
     wrap = false; # Prevent text from wrapping
   };
+  extraConfigLuaPost = ''
+    -- Highlight when yanking (copying) text
+    --  Try it with `yap` in normal mode
+    --  See `:help vim.highlight.on_yank()`
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      desc = "Highlight when yanking (copying) text",
+      group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+      callback = function()
+        vim.highlight.on_yank()
+      end,
+    })
+  '';
 }
