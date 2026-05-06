40c61576496f653b5daab2b0ae7417469e2da8b9
Nathan Graule <solarliner@gmail.com>
2026-05-06 19:15:15 +0200

    feat(neovim): auto reuse nvim instance in :term

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,3 +1,23 @@
-{
+{pkgs, ...}: {
   programs.nixvim.imports = [./nixvim/default.nix ../../modules/nixvim.nix];
+
+  home.packages = with pkgs; [neovim-remote];
+
+  programs.zsh.initContent = ''
+    if [[ "x''${NVIM}" != "x" ]]; then
+      function nvim_open {
+        nvr --servername "''${NVIM}" --remote-tab-wait "+set bufhidden=delete" ''${1}
+      }
+      function nvim_with_server {
+        nvr --servername "''${NVIM}" ''${@}
+      }
+      export VISUAL=nvim_open
+      export EDITOR=nvim_open
+      export GIT_EDITOR=nvim_open
+      alias v=nvim_with_server
+      alias vi=nvim_with_server
+      alias vim=nvim_with_server
+      alias nvim=nvim_with_server
+    fi
+  '';
 }
